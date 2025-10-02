19e0f9491e516e4bec5ced810f62e7fe6a0ab985
MARJHORIT <majhoritherrerasiguentes@gmail.com>
2025-10-02 10:46:43 -0500

    Archivo vista de bienvenido

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saludos', function (): string {
+    return 'Hello World';
+});
+
+Route::get('/bienvenido', function () {
+
+    return view( 'bienvenido');
+});
